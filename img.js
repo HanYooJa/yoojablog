ee3c16572c2feeeb648b648af7a33afcf7c5798f
HanYooJa <hyj03250@naver.com>
2023-06-11 20:29:52 +0900

    4

--- img.js
+++ img.js
@@ -1,5 +1,4 @@
 const current = document.getElementById('current')
-// const current = document.querySelector('#current');
 const imgs = document.querySelectorAll('.imgs img')
 
 imgs.forEach((img) => img.addEventListener('click', imgClick))
